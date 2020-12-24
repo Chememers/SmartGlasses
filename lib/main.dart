50197565ccf0137ed6296689bf94b644a1a2f6a9
3dcantaloupe <pranavdgs@gmail.com>
2020-12-23 18:29:01 -0800

    Added IndieFlower font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text('hey ninjas! '),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.green[800],
+        ),
+        body: Center(
+          child: Text(
+            'hello ninjas! ',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.lightGreen,
+              fontFamily: 'IndieFlower',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
     ));
