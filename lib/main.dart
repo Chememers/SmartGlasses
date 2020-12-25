b18ecfc36daa74e6eb352dac5fde6aacd0502b64
harirakul <harirakul.a@gmail.com>
2020-12-25 10:34:51 -0800

    started app

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.green[800],
+          title: Text("Smart Glasses"),
         ),
-        body: Center(
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: RaisedButton(
+          onPressed: () {},
           child: Text(
-            'hello ninjas! ',
+            "Connect",
             style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.lightGreen,
               fontFamily: 'IndieFlower',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
-          backgroundColor: Colors.red[600],
-        ),
+            )
+          )
+        )
       ),
-    ));
+    )
+  ));
