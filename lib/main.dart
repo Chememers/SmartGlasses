f49178c8ebf6cbb9af393b5d13a10bbafdaeacc8
harirakul <harirakul.a@gmail.com>
2020-12-31 14:10:04 -0800

    Using checkbox to toggle to BLE Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'BLEscan.dart';
 
 void main() => runApp(MaterialApp(
       //debugShowCheckedModeBanner: false,
       home: Home(),
     ));
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final List<BluetoothDevice> devices = new List<BluetoothDevice>();
-  addDevice(final BluetoothDevice device) {
-    if (!devices.contains(device)) {
-      setState(() {
-        devices.add(device);
-      });
-    }
-  }
-
-  void scanDevices() {
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-    flutterBlue.startScan(timeout: Duration(seconds: 4));
-
-    var subscription = flutterBlue.scanResults.listen((results) {
-      for (ScanResult r in results) {
-        addDevice(r.device);
-      }
-    });
-
-    flutterBlue.stopScan();
-  }
-
-  ListView buildListView() {
-    scanDevices();
-    List<Container> containers = new List<Container>();
-    for (BluetoothDevice device in devices) {
-      containers.add(Container(
-          height: 50,
-          child: Row(
-            children: <Widget>[
-              Column(children: [Text(device.name), Text(device.id.toString())],),
-              FlatButton(
-                color: Colors.blue,
-                child: Text('Connect', style: TextStyle(color: Colors.white)),
-                onPressed: () {},
-              )
-            ],
-          )));
-    }
-    return ListView(
-      children: <Widget>[
-        ...containers,
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Smart Glasses"),
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Smart Glasses"),
       ),
-      body: buildListView(),
+      body: new Checkbox(
+          value: false,
+          onChanged: (bool newValue) {
+            Navigator.push(
+              ctxt,
+              new MaterialPageRoute(builder: (ctxt) => new BLE()),
+            );
+          }
+      )
     );
   }
 }
